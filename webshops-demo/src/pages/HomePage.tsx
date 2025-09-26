4c88f59cc23755678dc9bee72a497713461ee309
Ivanov Dmitry <dmitry.ivanov.developer@gmail.com>
2025-09-26 11:44:33 +0300

    Renemed title

--- webshops-demo/src/pages/HomePage.tsx
+++ webshops-demo/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1 style={{marginBottom: 12}}>Welcome to WebShops Scenarios simulations</h1>
+      <h1 style={{marginBottom: 12}}>Interactive Specs</h1>
       <p style={{color: '#9fb3d9'}}>Explore store, rewards, news, updates, events and achievements.</p>
     </div>
   );
